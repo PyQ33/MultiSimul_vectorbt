362f0177a22957dab132f7f60d6f98f67a649df7
Oleg Polakow <olegpolakow@gmail.com>
2020-04-21 23:36:58 +0200

    Leave only documentation

--- vectorbt/__init__.py
+++
@@ -1,7 +0,0 @@
-from vectorbt import timeseries, signals, indicators, portfolio, widgets, utils, accessors
-
-from vectorbt.timeseries import *
-from vectorbt.signals import *
-from vectorbt.indicators import *
-from vectorbt.portfolio import *
-from vectorbt.widgets import *
