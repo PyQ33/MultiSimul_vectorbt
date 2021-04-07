3f11dcbedf1b791fa86e2b079d5516aaa2db00a5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-04-07 13:14:07 +0000

    Deploy polakowo/vectorbt to github.com/polakowo/vectorbt.git:gh-pages

--- vectorbt/returns/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Modules for working with returns.
-
-Offers common financial risk and performance metrics as found in [empyrical](https://github.com/quantopian/empyrical),
-but Numba-compiled and optimized for 2-dim arrays."""
-
